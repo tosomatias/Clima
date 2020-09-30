b58db7ecdd6f09c2b07ff0ec91e4217645c01fdc
matute <tosomatias@gmail.com>
2020-09-29 21:20:00 -0300

    Updates

---
+++ precache-manifest.7ea26feea7e5fff9cc2f6f9a2c5589a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "219b655d67846062d4835297e1ad7588",
+    "url": "/Clima/index.html"
+  },
+  {
+    "revision": "4b6be0a1c006b3c4d376",
+    "url": "/Clima/static/css/main.3e82f7ce.chunk.css"
+  },
+  {
+    "revision": "537430fb85c3bf217ce5",
+    "url": "/Clima/static/js/2.d814037f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Clima/static/js/2.d814037f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b6be0a1c006b3c4d376",
+    "url": "/Clima/static/js/main.7abe07cf.chunk.js"
+  },
+  {
+    "revision": "aaa7f9f5c67068460101",
+    "url": "/Clima/static/js/runtime-main.5bf0fa65.js"
+  }
+]);
